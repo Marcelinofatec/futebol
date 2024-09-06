84a4b4ac616db10d900eb0b462d2ab72eed4d47f
Marcelinofatec <118126630+Marcelinofatec@users.noreply.github.com>
2024-09-06 20:47:04 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Flamengo",
+        descricao: "Conhecido como o Mengão, é um dos clubes mais populares do Brasil e da América Latina. Possui uma torcida apaixonada e um histórico vitorioso em diversas competições.",
+        link: "https://www.flamengo.com.br/",
+        tags: "flamengo mengão "
+    },
+    {
+        titulo: "Corinthians",
+        descricao: " O Timão é outro gigante do futebol brasileiro, com uma torcida fiel e um estilo de jogo aguerrido. Tem conquistado diversos títulos nacionais e internacionais..",
+        link: "https://www.corinthians.com.br/",
+        tags: "timao gavioes fiel"
+    },
+    {
+        titulo: "São Paulo",
+        descricao: " O Tricolor é um dos clubes mais tradicionais do país, com uma base de torcedores sólida e um histórico de conquistas importantes.",
+        link: "https://www.saopaulofc.net/",
+        tags: "tricolor "
+    }
+];
